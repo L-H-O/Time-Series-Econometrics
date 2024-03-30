86622d6f0e4903f278954d9bf3c8e42140124cf5
Lucas Oliveira <85700941+L-H-O@users.noreply.github.com>
2024-03-30 10:15:07 -0300

    add missing controls

--- working.py
+++ working.py
@@ -18,8 +18,11 @@ os.chdir(home)
 
 raw_inf = pd.read_csv("infant-mortality.csv")
 raw_dem = pd.read_csv("V-Dem-CY-Full+Others-v14.csv", low_memory = False)
+raw_pop = pd.read_csv("population.csv", low_memory = False)
+
 raw_pwt = pd.read_excel("pwt1001.xlsx", sheet_name = 'Data')
 raw_urb = pd.read_csv("urb-growth.csv", header = 2)
+raw_fer = pd.read_csv("fertility-rate.csv", header = 2)
 
 raw_gdppc = pd.read_html("http://www.ipeadata.gov.br/ExibeSerie.aspx?serid=38375")[2]
 
@@ -34,6 +37,15 @@ df_inf = (raw_inf
         )
 
 df_dem = raw_dem.query("country_name == 'Brazil' and 1934 <= year <= 2021")
+
+df_pop = (raw_pop
+                .query("ISO3_code == 'BRA' and Variant == 'Medium'")
+                [["Time", "PopTotal"]]
+                .assign(Year = lambda df_ : pd.to_datetime(df_["Time"],
+                                                           format = "%Y"))
+                .drop(columns = 'Time')
+                .sort_values(by = 'Year')
+                )
 
 df_pwt = (raw_pwt
                  .query("country == 'Brazil'")
@@ -57,6 +69,19 @@ df_urb = (raw_urb
                  .rename(columns = {29 :'urb_growth'})
                  )
 
+df_fer = (raw_fer
+                 .query("`Country Name` == 'Brazil'")
+                 .T
+                 .reset_index()
+                 .iloc[4:]
+                 .dropna()
+                 .assign(Year = lambda df_ : pd.to_datetime(df_["index"],
+                                                            format = "%Y")
+                 )
+                 .drop(columns = 'index')
+                 .rename(columns = {29 :'fer_rate'})
+                 )
+
 df_gdppc = (raw_gdppc
                      .iloc[1:]
                      .assign(Year = lambda df_ : pd.to_datetime(df_[0],
@@ -68,8 +93,8 @@ df_gdppc = (raw_gdppc
 
 data =  (df_dem
                 .assign(Year = lambda df_ : pd.to_datetime(df_["historical_date"]))
-                [["Year", "v2caviol", "v2x_accountability", "v2x_rule",
-                  "v2x_corr", "v2xcs_ccsi" ]]
+                [["Year", "v2caviol", "v2x_accountability", "v2x_accountability_osp",
+                  "v2x_rule", "v2x_corr", "v2xcs_ccsi" ]]
                 .set_index('Year')
                 .resample('YS')
                 .first()
@@ -82,13 +107,19 @@ data =  (df_dem
                       on = 'Year')
                 .merge(df_gdppc,
                        on = 'Year')
+                .merge(df_fer,
+                       on = 'Year')
+                .merge(df_pop,
+                       on = 'Year')
                 .set_index('Year')
-                .set_axis(['pol_violence', 'accountability', "rule_of_law"
+                .set_axis(['pol_violence', 'accountability',
+                           'accountability_scaled', "rule_of_law"
                            ,"corruption", 'civil_society', 'imr',
-                           'human_capital', "urb_growth", "gdp_pc"]
+                           'human_capital', "urb_growth", "gdp_pc",
+                           "fer_rate", "pop"]
                           , axis = 1)
                 )
                 
-#%%
 
-data.plot(secondary_y = 'gdp_pc')
+
+df_dem.f
